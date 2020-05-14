8ec88bf49bbce6cc9c5ae7815e5654c2fae4598b
Ryan Werner <rmwerner@umich.edu>
2020-05-14 00:11:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-function addElementToBeginningOfArray(array, element) {return element, ...array};
+function addElementToBeginningOfArray(array, element) {return [element, ...array]};
 function destructivelyAddElementToEndOfArray(array,element) {return array.unshift(element)};
 function addElementToEndOfArray(array, element) {return [...array, element]};
 function destructivelyAddElementToEndOfArray (array, element) {return array.push(element)};
